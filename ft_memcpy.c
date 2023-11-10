b490bfe1ad46305609fc0fa6033c86526deb9f14
emre149 <ededemog@student.42.fr>
2023-11-10 17:16:31 +0100

    memcpy a corriger

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,21 +6,40 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 13:05:23 by ededemog          #+#    #+#             */
-/*   Updated: 2023/11/10 16:35:42 by ededemog         ###   ########.fr       */
+/*   Updated: 2023/11/10 16:58:44 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	*memcpy(void * dest, const void *src, size_t size)
+#include "libft.h"
+
+void	*ft_memcpy(void *dest, const void *src, size_t size)
 {
 	void	*dest_temp;
 	void	*src_temp;
 
 	if (!src && !dest)
-		return (dest)
+		return (dest);
 	dest_temp = (unsigned char *)dest;
 	src_temp = (unsigned char *)src;
-	while (dest_temp <= size)
+	while (size > 0)
 	{
-		dest_temp = src_temp; 
+		*(dest_temp++) = *(src_temp++);
+		size--;
 	}
+	return (dest);
+}
+
+#include <stdio.h>
+
+int main() {
+    char source[] = "Bonjour!";
+    char destination[20];
+
+    // Utilisez votre fonction my_memcpy ici
+    ft_memcpy(destination, source, ft_strlen(source) + 1);
+
+    printf("Source: %s\n", source);
+    printf("Destination: %s\n", destination);
+
+    return 0;
 }
