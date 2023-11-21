819a9c164a57de80d1cff079c309c0e675383ba6
emre149 <ededemog@student.42.fr>
2023-11-21 02:42:59 +0100

    calloc

--- ft_calloc.c
+++ ft_calloc.c
@@ -1,41 +1,29 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memset.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/07 10:42:50 by ededemog          #+#    #+#             */
-/*   Updated: 2023/11/21 01:29:24 by ededemog         ###   ########.fr       */
+/*   Created: 2023/11/21 01:32:15 by ededemog          #+#    #+#             */
+/*   Updated: 2023/11/21 02:42:45 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_memset(void *pointer, int value, size_t count)
+void	*ft_calloc(size_t count, size_t size)
 {
-	size_t	i;
+	unsigned char	*temp;
+	size_t			i;
 
-	i = 0;
-	while (i < count)
+	temp = malloc(count * size);
+	if (!tmp)
+		return (NULL);
+	while (i < count * size)
 	{
-		*(unsigned char *)(pointer + i) = (unsigned char)value;
+		temp[i] == 0;
 		i++;
 	}
-	return (pointer);
+	return (temp);
 }
-/*
-#include <stdio.h>
-
-int	main()
-{
-	char mem[50];
-	ft_memset(mem, 'x', 50);
-	ft_memset(mem, 'a', 10);
-	for (int i = 0; i < 50; i++)
-		printf("%c", mem[i]);
-	printf("\n");
-	//printf("%s\n", mem);
-	return 0;
-}
-*/
