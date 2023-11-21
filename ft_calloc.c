56c64ddfaf36e44e0173c29cf7e7b07f19339968
emre149 <ededemog@student.42.fr>
2023-11-21 04:01:21 +0100

    correction calloc de con

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: ededemog <ededemog@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/21 01:32:15 by ededemog          #+#    #+#             */
-/*   Updated: 2023/11/21 02:42:45 by ededemog         ###   ########.fr       */
+/*   Updated: 2023/11/21 04:01:00 by ededemog         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@ void	*ft_calloc(size_t count, size_t size)
 	unsigned char	*temp;
 	size_t			i;
 
+	i = 0;
 	temp = malloc(count * size);
 	if (!tmp)
 		return (NULL);
